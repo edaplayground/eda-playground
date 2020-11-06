8ec730ecac205f24eed129d68ff65bcba279dd3b
EDA Playground <getedaplayground@gmail.com>
2020-11-06 16:10:44 +0000

    updated for 2020

--- docs/conf.py
+++ docs/conf.py
@@ -45,7 +45,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'EDA Playground'
-copyright = u'2015, Doulos'
+copyright = u'2020, Doulos'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
