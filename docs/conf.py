0f4bff8bb098c9795c555996d099e2317de0a851
EDA Playground <getedaplayground@gmail.com>
2023-06-14 14:03:16 +0100

    commented out: #html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@
 
 import sys
 import os
-#import sphinx_bootstrap_theme
+import sphinx_bootstrap_theme
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -159,7 +159,7 @@ html_theme_options = {
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
+#html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
